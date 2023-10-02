5d821f90acd92d56ea5390dcb9188119ab50093b
SHAIK SHABANA <111629425+shabsshaik@users.noreply.github.com>
2023-10-02 23:20:40 +0530

    Add files via upload

---
+++ python_questions.js
@@ -0,0 +1,132 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+  {
+  numb: 1,
+  question: "Python is an",
+  answer: "All the above",
+  options: [
+    " interpreted, high-level, general-purpose programming language",
+    "object-oriented programming language",
+    "procedural, functional, structured, and reflective programming language",
+    "All the above"
+  ]
+},
+
+
+
+{
+  numb: 2,
+  question: " To shuffle the list say mylist, what function do we use ?",
+  answer: "random.shuffle(mylist)",
+  options: [
+    "random.shuffleList(mylist)",
+    "mylist.shuffle()",
+    "shuffle(mylist)",
+    "random.shuffle(mylist)"
+  ]
+},
+{
+  numb: 3,
+  question: "What is the output of the following program ?mylist = [10, 20, 5, 30, 15] print(mylist[:-1])",
+  answer: "[10, 20, 5, 30]",
+  options: [
+    "[10, 20, 5, 30]",
+    "[10, 20, 5, 30, 15]",
+    "[15, 20, 5, 30, 10",
+    "Error"
+  ]
+},
+{
+  numb: 4,
+  question: "What will be the output produced by following program ? mylist = [10, 20, 5, 30, 15] print(mylist * 2)",
+  answer: "[10, 20, 5, 30, 15, 10, 20, 5, 30, 15]",
+  options: [
+    "[20, 30]",
+    "[10,5, 30, 15]",
+    "[20,10, 60, 30]",
+    "[10, 20, 5, 30, 15, 10, 20, 5, 30, 15]"
+  ]
+},
+{
+  numb: 5,
+  question: "Which of the following is an invalid statement ?",
+  answer: " a b c = 1000 2000 3000",
+  options: [
+    "a_b_c = 1,000,000",
+    "a,b,c = 1000, 2000, 3000",
+    " a b c = 1000 2000 3000",
+    "abc = 1,000,000"
+  ]
+},
+{
+  numb: 6,
+  question: "Which are the advantages of functions in python ?",
+  answer: "All of the Above",
+  options: [
+    "Reducing duplication of code",
+    "Decomposing complex problems into simpler pieces",
+    "Reuse of code",
+    "All of the Above"
+  ]
+},
+{
+  numb: 7,
+  question: "What is called when a function is defined inside a class ?",
+  answer: "Method",
+  options: [
+    "Method",
+    "Another function",
+    "Class",
+    "Module"
+  ]
+},
+{
+numb: 8,
+question: "Which of the following is the use of id() function in python ?",
+answer: "All of the Above",
+options: [
+  "Every object does not have a unique id",
+  "Id returns the identity of the object",
+  "All of the Above",
+  "None of the above"
+]
+},
+{
+numb: 9,
+question: "Which of the following refers to mathematical function ?",
+answer: "sqrt()",
+options: [
+  "rhombus()",
+  "add()",
+  "rhombus()",
+  "sqrt()"
+]
+},
+
+{
+numb: 10,
+question: "Python supports the creation of anonymous functions at runtime, using a construct called",
+answer: "Lambda",
+options: [
+  "anonymous",
+  "Lambda",
+  "pi",
+  "None of the above"
+]
+},
+
+// you can uncomment the below codes and make duplicate as more as you want to add question
+// but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
+
+//   {
+//   numb: 6,
+//   question: "Your Question is Here",
+//   answer: "Correct answer of the question is here",
+//   options: [
+//     "Option 1",
+//     "option 2",
+//     "option 3",
+//     "option 4"
+//   ]
+// },
+];
